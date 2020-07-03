acee66ce4785975e593dd274fccde4b4e27441ed
Jay Prajapati <jayprajapati9900@gmail.com>
2020-07-03 11:41:36 +0530

    Edged Image Function Added

--- Basic_Functions.py
+++ Basic_Functions.py
@@ -11,10 +11,14 @@ img = cv2.imread("Resources/Lena.jpg")
 imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 # To Blur the Image
 blurImg = cv2.GaussianBlur(imgGray, (7,7), 0)
+# Edge Detector 
+imgCanny = cv2.Canny(img, 100, 100)
 
 # Showiing the converted image
 cv2.imshow("Gray Scale Imae", imgGray)
 # Showing the Blurred Image 
 cv2.imshow("Blurred Image", blurImg)
+# Showing the Edged Image
+cv2.imshow("Edged Image", imgCanny)
 
 cv2.waitKey(0)
