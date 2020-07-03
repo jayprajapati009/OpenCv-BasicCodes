9bcd91ffb76b76da253f32a717dbe2b21c6009f8
Jay Prajapati <jayprajapati9900@gmail.com>
2020-07-03 11:13:07 +0530

    Functions 1

---
+++ Basic_Functions.py
@@ -0,0 +1,20 @@
+# This code is for different basic functions
+
+# Importing Library 
+import cv2
+print("Library Imported")
+
+# Reading the Image
+img = cv2.imread("Resources/Lena.jpg") 
+
+# To Convert the image from BGR scale to Gray Scale
+imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+# To Blur the Image
+blurImg = cv2.GaussianBlur(imgGray, (7,7), 0)
+
+# Showiing the converted image
+cv2.imshow("Gray Scale Imae", imgGray)
+# Showing the Blurred Image 
+cv2.imshow("Blurred Image", blurImg)
+
+cv2.waitKey(0)
