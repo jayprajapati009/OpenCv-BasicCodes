06afe66f293b0efec5bc2d6fb8cbadfc721e810d
Jay Prajapati <jayprajapati9900@gmail.com>
2020-07-06 08:36:37 +0530

    Final Commit

--- Basic_Functions.py
+++ Basic_Functions.py
@@ -2,6 +2,7 @@
 
 # Importing Library 
 import cv2
+import numpy as np
 print("Library Imported")
 
 # Reading the Image
@@ -13,6 +14,14 @@ imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 blurImg = cv2.GaussianBlur(imgGray, (7,7), 0)
 # Edge Detector 
 imgCanny = cv2.Canny(img, 100, 100)
+# To reduce the edges
+imgCanny2 = cv2.Canny(img, 150, 200)
+# To dilate Image (Edges becomes Thick)
+# A kernel is a matrix to define the image
+kernel = np.ones((5, 5),np.uint8)
+imgDilation = cv2.dilate(imgCanny, kernel, iterations=1)
+# To Erode image (Edges becomes thin)
+imgErode = cv2.erode(imgDilation, kernel, iterations=1)
 
 # Showiing the converted image
 cv2.imshow("Gray Scale Imae", imgGray)
@@ -20,5 +29,11 @@ cv2.imshow("Gray Scale Imae", imgGray)
 cv2.imshow("Blurred Image", blurImg)
 # Showing the Edged Image
 cv2.imshow("Edged Image", imgCanny)
+# Showing the second Edged Image
+cv2.imshow("Reduced Edge Image", imgCanny2)
+# Showing Dilated Image
+cv2.imshow("Dilated Image", imgDilation)
+# Showing the eroded image
+cv2.imshow("Eroded Image", imgErode)
 
 cv2.waitKey(0)
